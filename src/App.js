90b3a229005c61e95a4a0f71902bc01b552930e7
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-25 17:10:19 +0200

    Basic - Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
