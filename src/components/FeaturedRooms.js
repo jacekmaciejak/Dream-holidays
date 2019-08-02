b72e98cde108f1584745fe95da74c031f77adff6
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-08-02 16:31:50 +0200

    Loading comp added

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     const { FeaturedRooms: rooms } = this.context;
-    return <div>Hello from featured rooms</div>;
+    return (
+      <div>
+        Hello from featured rooms
+        <Loading />
+      </div>
+    );
   }
 }
