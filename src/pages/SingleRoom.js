2061ddb1c69e5136f15784cebc06923148800b5a
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-05-25 19:52:33 +0200

    Subpages structure has been added

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,6 +4,7 @@ import Banner from "../components/Banner/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
 import StyledHero from "../components/StyledHero/StyledHero";
+import { GiEarthAsiaOceania } from "react-icons/gi";
 
 export default class SingleRoom extends Component {
   constructor(props) {
@@ -56,6 +57,28 @@ export default class SingleRoom extends Component {
               <img key={index} src={item} alt={name} />
             ))}
           </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: ${price}</h6>
+              <h6>size: ${size} SQFT</h6>
+              <h6>max capacity: {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}><span><GiEarthAsiaOceania /></span> {item}</li>
+            })}
+          </ul>
         </section>
       </>
     );
