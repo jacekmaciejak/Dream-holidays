13149174a35c44fc7cbfcd3de9c79f43a81b1343
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-05-26 17:37:44 +0200

    RoomList comp. has been added

--- src/components/RoomsContainer/RoomsContainer.js
+++ src/components/RoomsContainer/RoomsContainer.js
@@ -2,30 +2,56 @@ import React from 'react'
 import './RoomsContainer.css'
 import RoomsFilter from './RoomsFilter/RoomsFilter'
 import RoomsList from './RoomsList/RoomsList'
-import { RoomConsumer } from '../../context'
+import { withRoomConsumer } from '../../context'
 import Loading from '../Loading/Loading'
 
-function RoomsContainer() {
+function RoomsContainer({ context }) {
+    const { loading, sortedRooms, rooms } = context;
+    if (loading) {
+        return <Loading />
+    }
     return (
-        <RoomConsumer>
-            {
-                (value) => {
-                    const { loading, sortedRooms, rooms } = value;
-                    if (loading) {
-                        return <Loading />
-                    }
-                    return (
-                        <div>
-                            <h1>hello from rooms container</h1>
-                            <RoomsFilter rooms={rooms}></RoomsFilter>
-                            <RoomsList romms={sortedRooms}></RoomsList>
-                        </div>
-                    )
-                }
-            }
-        </RoomConsumer>
-
+        <>
+            <RoomsFilter rooms={rooms}></RoomsFilter>
+            <RoomsList rooms={sortedRooms}></RoomsList>
+        </>
     )
 }
 
-export default RoomsContainer
+export default withRoomConsumer(RoomsContainer);
+
+
+
+
+
+// import React from 'react'
+// import './RoomsContainer.css'
+// import RoomsFilter from './RoomsFilter/RoomsFilter'
+// import RoomsList from './RoomsList/RoomsList'
+// import { RoomConsumer } from '../../context'
+// import Loading from '../Loading/Loading'
+
+// function RoomsContainer() {
+//     return (
+//         <RoomConsumer>
+//             {
+//                 (value) => {
+//                     const { loading, sortedRooms, rooms } = value;
+//                     if (loading) {
+//                         return <Loading />
+//                     }
+//                     return (
+//                         <div>
+//                             <h1>hello from rooms container</h1>
+//                             <RoomsFilter rooms={rooms}></RoomsFilter>
+//                             <RoomsList romms={sortedRooms}></RoomsList>
+//                         </div>
+//                     )
+//                 }
+//             }
+//         </RoomConsumer>
+
+//     )
+// }
+
+// export default RoomsContainer
--- src/components/RoomsContainer/RoomsList/RoomsList.js
+++ src/components/RoomsContainer/RoomsList/RoomsList.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Room from '../../Room/Room'
 import './RoomsList.css'
 
-function RoomsList() {
+function RoomsList({ rooms }) {
+
+    if (rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h2>brak lokalizacji spełniających wybrane parametry wyszukiwania</h2>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>hello from rooms list</h1>
-        </div>
+        <section className="rooms-list">
+            <div className="rooms-list-center">
+                {rooms.map(item => {
+                    return <Room key={item.id} room={item} />
+                })}
+            </div>
+        </section>
     )
 }
 
