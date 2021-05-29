e90f6c3230e7f4a8bfdd9a409250ca39b7578897
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-05-29 20:47:13 +0200

    Breakfast & pets filter have been added

--- src/context.js
+++ src/context.js
@@ -55,7 +55,7 @@ class RoomProvider extends Component {
   };
   handleChange = e => {
     const target = e.target;
-    const value = e.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = e.target.name;
     this.setState({
       [name]: value
@@ -82,6 +82,14 @@ class RoomProvider extends Component {
     tempRooms = tempRooms.filter(room => room.price <= price)
     //filter by size
     tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
 
     //change state
     this.setState({
