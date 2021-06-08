34d8b6400744283ad6e682a5fcac796579958c8e
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-06-08 20:13:18 +0200

    Updates

---
+++ precache-manifest.5f9e116f6e1a6c7e4be401f6a851dc65.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0865968a8da7f695e8807f4736c3a2ff",
+    "url": "/Dream-holidays/index.html"
+  },
+  {
+    "revision": "9db387be25389938e9cc",
+    "url": "/Dream-holidays/static/css/main.62a2ecd0.chunk.css"
+  },
+  {
+    "revision": "39ee3a37b984c3e5e712",
+    "url": "/Dream-holidays/static/js/2.f604cafe.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/Dream-holidays/static/js/2.f604cafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db387be25389938e9cc",
+    "url": "/Dream-holidays/static/js/main.078b6d97.chunk.js"
+  },
+  {
+    "revision": "9a2ddb26dbf3dbb6c370",
+    "url": "/Dream-holidays/static/js/runtime-main.732c4003.js"
+  },
+  {
+    "revision": "b2d36bdb96d2c93dc6312f6d39be0651",
+    "url": "/Dream-holidays/static/media/contact.b2d36bdb.jpg"
+  },
+  {
+    "revision": "b358941aa227101a2fc6cb38386b6cf9",
+    "url": "/Dream-holidays/static/media/defaultBcg.b358941a.jpg"
+  },
+  {
+    "revision": "8e0c64ef46bbfa21fb38eb4508079c86",
+    "url": "/Dream-holidays/static/media/defaultImg.8e0c64ef.jpg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/Dream-holidays/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/Dream-holidays/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/Dream-holidays/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/Dream-holidays/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/Dream-holidays/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "32965472dbcab4538ff106918a27f16b",
+    "url": "/Dream-holidays/static/media/madagaskar.32965472.jpg"
+  },
+  {
+    "revision": "7e0b9be24a69eca0026264be61327b81",
+    "url": "/Dream-holidays/static/media/madagaskar_1.7e0b9be2.jpg"
+  },
+  {
+    "revision": "b8f3a54df045bc9ac33128d097fdfa21",
+    "url": "/Dream-holidays/static/media/malediwy.b8f3a54d.jpg"
+  },
+  {
+    "revision": "084e464ab61907da1105dfb716d4f3a8",
+    "url": "/Dream-holidays/static/media/mauritius.084e464a.jpg"
+  },
+  {
+    "revision": "46bb13c7d44362f4b1ccf64071abc2f1",
+    "url": "/Dream-holidays/static/media/phuket.46bb13c7.jpg"
+  },
+  {
+    "revision": "491ac1a4f969d5cf215cc838ca370a32",
+    "url": "/Dream-holidays/static/media/plane.491ac1a4.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/Dream-holidays/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/Dream-holidays/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/Dream-holidays/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/Dream-holidays/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/Dream-holidays/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/Dream-holidays/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/Dream-holidays/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/Dream-holidays/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/Dream-holidays/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/Dream-holidays/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/Dream-holidays/static/media/room-9.7b17c49f.jpeg"
+  },
+  {
+    "revision": "b71566dd4226a9358a97d612e337cdb6",
+    "url": "/Dream-holidays/static/media/sri_lanka.b71566dd.jpg"
+  },
+  {
+    "revision": "f0b94a0becc9188aff034fcfd775d422",
+    "url": "/Dream-holidays/static/media/zanzibar.f0b94a0b.jpg"
+  }
+]);
